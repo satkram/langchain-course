41e2dd25ecbc7564693f0de4b5c194af540c1eef
satkram <100255063+satkram@users.noreply.github.com>
2026-05-17 23:14:04 -0400

    Ollama gemma3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -21,7 +22,8 @@ def main():
 
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-    llm = ChatOpenAI(model="gpt-5", temperature=0)
+    #llm = ChatOpenAI(model="gpt-5", temperature=0)
+    llm = ChatOllama(model="gemma3:270m", temperature=0)
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
     print(response.content)
